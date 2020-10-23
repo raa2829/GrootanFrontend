cd866568f521406350a79f6fb63d6f73bbe3937a
raa2829 <aravindmnnit123@gmail.com>
2020-10-23 14:59:23 +0530

    Updates

---
+++ precache-manifest.e48c897cfbbdec75038bde94d61dba6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfc1df772beec04726c46a7590c43c2",
+    "url": "/GrootanFrontend/index.html"
+  },
+  {
+    "revision": "679d840c42abfd2fe2b5",
+    "url": "/GrootanFrontend/static/css/main.23bd8ef5.chunk.css"
+  },
+  {
+    "revision": "ad88b2159ec5b22eadad",
+    "url": "/GrootanFrontend/static/js/2.e15c7ae8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/GrootanFrontend/static/js/2.e15c7ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679d840c42abfd2fe2b5",
+    "url": "/GrootanFrontend/static/js/main.ecf8fac6.chunk.js"
+  },
+  {
+    "revision": "6223f4301b6f8718635a",
+    "url": "/GrootanFrontend/static/js/runtime-main.aa67c2cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GrootanFrontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
